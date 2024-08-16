eaa2bf3d3f696401693cb45adad84841237a1a17
DungNguyenManh <dung903698954@gmail.com>
2024-08-16 15:50:50 +0700

    video 19

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const path = require('path');
+require('dotenv').config();
+
+console.log(">>> check env: ", process.env);
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 8888;
+const hostname = process.env.HOST_NAME;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -20,6 +24,6 @@ app.get('/hoidanit', (req, res) => {
     res.render('sample.ejs')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+app.listen(port, hostname || 'localhost', () => {
+    console.log(`Example app listening on http://${hostname || 'localhost'}:${port}`)
+});
